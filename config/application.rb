81c1cafcef644d30661604d321f3bf599e85254f
slawagn <slawagn@gmail.com>
2022-08-27 10:35:46 +0000

    replace available_locales with default_locale

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module BbqRails6
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.available_locales = :ru
+    config.i18n.default_locale = :ru
   end
 end
