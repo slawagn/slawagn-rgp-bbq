c9912e297de221d9197516c1b43e51f0e651c5fd
slawagn <slawagn@gmail.com>
2022-08-31 13:44:10 +0000

    replace inline check with current_user_can_subscribe? helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+  def current_user_can_subscribe?(event)
+    if current_user == event.user
+      false
+    elsif event.subscribers.include?(current_user)
+      false
+    else
+      true
+    end
+  end
 end
