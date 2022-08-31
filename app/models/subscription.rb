a817e634213b930ba34391d5a4fd69604c7ab314
slawagn <slawagn@gmail.com>
2022-08-31 16:23:41 +0000

    forbid subscribing using email of an existing user

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,6 +9,8 @@ class Subscription < ApplicationRecord
 
     validates :user_email,
       uniqueness: { scope: :event_id }
+
+    validate :using_email_of_existing_user
   end
 
   with_options if: -> { user.present? } do
@@ -39,6 +41,12 @@ class Subscription < ApplicationRecord
   def subscribing_to_own_event
     if event.user == user
       errors.add(:base, :subscribing_to_own_event)
+    end
+  end
+
+  def using_email_of_existing_user
+    if User.exists?(email: user_email.downcase)
+      errors.add(:user_email, :using_email_of_existing_user)
     end
   end
 end
