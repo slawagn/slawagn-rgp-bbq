c996a8d2394bc4009cda8725c0e12310c26c6042
slawagn <slawagn@gmail.com>
2022-09-02 21:35:21 +0300

    replace s3 host with an endpoint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
       region:                ENV['S3_REGION'],
-      host:                  ENV['S3_HOST']
+      endpoint:              ENV['S3_ENDPOINT']
     }
 
     config.fog_directory   = ENV['S3_BUCKET_NAME'] 
