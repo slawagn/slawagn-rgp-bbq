5ed807306ea1172130b008c57e7a6200eff8ee20
slawagn <slawagn@gmail.com>
2022-09-25 10:26:54 +0000

    fix imposstible to delete events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
-  has_many :comments
-  has_many :subscriptions
+  has_many :comments,      dependent: :destroy
+  has_many :photos,        dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
   has_many :subscribers,
     through: :subscriptions,
     source:  :user
-  has_many :photos
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
