f5d10a022fef8d60769904b8a3d58002709da338
slawagn <slawagn@gmail.com>
2022-09-25 10:28:44 +0000

    set same pundit_user for the whole ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def pundit_user
+    EventPolicy::UserWithPincode.new(current_user, cookies.permanent["event_#{@event&.id}_pincode"])
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -15,6 +19,7 @@ class ApplicationController < ActionController::Base
   end 
 
   add_flash_types :danger, :info, :warning, :success, :messages
+
 
   private
 
